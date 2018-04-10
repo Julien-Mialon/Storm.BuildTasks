9f8babe55dd7a51041d202a262ed94c7d4483d0b
Valentin Jubert <valentin.jubert@ideine.fr>
2018-04-10 12:02:18 +0200

    Component colors

---
+++ src/Storm.BuildTasks.ComponentColors/Colors.Android/ComponentColorsAndroidTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Colors.Core;
+
+namespace Colors.Android
+{
+	public class ComponentColorsAndroidTask : BaseLocalizationTask
+	{
+		public ComponentColorsAndroidTask()
+		{
+		}
+
+		public override bool Execute()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Storm.BuildTasks.ComponentColors/Colors.Core/BaseLocalizationTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace Colors.Core
+{
+	public abstract class BaseLocalizationTask : Task
+	{
+		public BaseLocalizationTask()
+		{
+		}
+	}
+}
